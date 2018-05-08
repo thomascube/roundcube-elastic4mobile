d6dd004ed699ee9257f529fa0c73437869edc229
Thomas Bruederli <thomas@roundcube.net>
2018-05-08 13:13:26 +0200

    Initial import

---
+++ README.md
@@ -0,0 +1,18 @@
+# Roundcube Elastic4Mobile plugin
+
+Roundcube Plugin to enforce the elastic skin if a mobile device is detected as client.
+
+Download and install via https://plugins.roundcube.net
+
+## License
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by 
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+[GNU General Public License](http://www.gnu.org/licenses/) for
+more details.
---
+++ elastic4mobile.php
@@ -0,0 +1,33 @@
+<?php
+
+/**
+ * Elastic skin for mobile devices plugin
+ *
+ * Selects the elastic skin if a mobile device is detected as client.
+ *
+ * @author Thomas Bruederli <thomas@roundcube.net>
+ * @license GNU GPLv3+
+ */
+class elastic4mobile extends rcube_plugin
+{
+    public $noajax = true;
+    public $task = '?(?!logout).*';
+
+    public function init()
+    {
+        $rcmail = rcmail::get_instance();
+        $detect = new Mobile_Detect;
+
+        if ($detect->isMobile() || $detect->isTablet()) {
+          $skin = 'elastic';
+          $rcmail->config->set('skin', $skin);
+          $rcmail->output->set_env('skin', $skin);
+          $rcmail->output->set_skin($skin);
+
+          // disable skin switch as this wouldn't have any effect
+          $dont_override = $rcmail->config->get('dont_override', []);
+          $dont_override[] = 'skin';
+          $rcmail->config->set('dont_override', $dont_override);
+        }
+    }
+}
